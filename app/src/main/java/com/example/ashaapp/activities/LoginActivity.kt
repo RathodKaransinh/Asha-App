83e74983fd37831dcd2697dce6dbfc2d040388b1
rathod_karansinh <rathodkaransinh125@gmail.com>
2023-03-11 18:02:31 -0500

    updated login design

--- app/src/main/java/com/example/ashaapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/ashaapp/activities/LoginActivity.kt
@@ -1,13 +1,11 @@
 package com.example.ashaapp.activities
 
 import android.animation.Animator
-import android.content.ContentValues
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
